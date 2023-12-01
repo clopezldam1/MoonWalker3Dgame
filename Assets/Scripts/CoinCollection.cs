5580458638036d68bc62cff7e5ff8e75f3851743
clopezldam1 <clopezldam1@gmail.com>
2023-12-01 11:44:48 +0100

    arreglada gravedad y añadido metodo para cambiar a you won

--- Assets/Scripts/CoinCollection.cs
+++ Assets/Scripts/CoinCollection.cs
@@ -6,15 +6,23 @@ using UnityEngine;
 public class CoinCollection : MonoBehaviour
 {
     private int coins = 0;
+    public const int maxCoins = 10;
     public TextMeshProUGUI contadorCoins;
+
+    private void Update()
+    {
+        if(coins == maxCoins)
+        {
+            SceneController.cambiarEscena(2);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Coin")
         {
             coins++;
-            contadorCoins.text = coins.ToString() + " / 10";
-            print(coins);
+            contadorCoins.text = coins.ToString() + " / " + maxCoins.ToString();
             Destroy(other.gameObject);
         }
     }
